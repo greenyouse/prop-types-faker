f020e15abccdc8ce250fcea950e6eb71a8940e82
Ed Babcock <ed@edbabcock.com>
2019-08-04 11:34:33 -0500

    fix: export npm package properly

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,17 @@ const path = require('path');
 module.exports = {
   entry: './src/faker.js',
   output: {
+    library: 'propTypesFaker',
+    libraryTarget: 'umd',
     filename: 'faker.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
-      { test: /.js$/, use: 'babel-loader' },
+      {
+        test: /.js$/,
+        use: 'babel-loader',
+      },
     ],
   },
 };
